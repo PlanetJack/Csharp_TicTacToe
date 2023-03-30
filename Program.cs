af875e5d45dfe74aad89f1f1e161ad07743003f4
PlanetJack <122494246+PlanetJack@users.noreply.github.com>
2023-03-30 13:52:49 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_ConsoleGame
+{
+    class Program
+    {
+        // the playField
+        static char[,] playField =
+        {
+            {'1', '2', '3'}, // Row 0
+            {'4', '5', '6'}, // Row 1
+            {'7', '8', '9'}, // Row 2
+        };
+
+
+
+        static void Main(string[] args)
+        {
+            int player = 2; //Player 1 starts
+            int input = 0;
+            bool inputCorrect = true;
+
+            // Run code as long as the program runs
+            do
+            {
+                if (player == 2)
+                {
+                    player = 1;
+                    EnterXorO(player, input);
+                }
+                else if (player == 1)
+                {
+                    player = 2;
+                    EnterXorO(player, input);
+                }
+
+                SetField();
+
+                do
+                {
+                    Console.Write("\nPlayer {0} : Choose your field! ", player);
+                    input = Convert.ToInt32(Console.ReadLine());
+
+                } while (!inputCorrect);
+
+                Console.ReadKey();
+
+            } while (true);
+        
+        }
+
+        public static void SetField()
+        {
+            Console.Clear();
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[0,0], playField[0,1], playField[0,2]);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[1, 0], playField[1, 1], playField[1, 2]);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[2, 0], playField[2, 1], playField[2, 2]);
+            Console.WriteLine("_____|_____|_____");
+        
+        }
+
+        public static void EnterXorO(int player, int input)
+        {
+            char playerSign = ' ';
+
+            if (player == 1)
+                playerSign = 'X';
+            if (player == 2)    
+                playerSign = 'O';
+
+
+
+            switch (input)
+            {
+                case 1: playField[0, 0] = playerSign; break;
+                case 2: playField[0, 1] = playerSign; break;
+                case 3: playField[0, 2] = playerSign; break;
+                case 4: playField[1, 0] = playerSign; break;
+                case 5: playField[1, 1] = playerSign; break;
+                case 6: playField[1, 2] = playerSign; break;
+                case 7: playField[2, 0] = playerSign; break;
+                case 8: playField[2, 1] = playerSign; break;
+                case 9: playField[2, 2] = playerSign; break;
+            }
+           
+        }
+    }
+}
